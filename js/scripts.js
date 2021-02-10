e93fcaa02a557daec18b26baf7a2b2f6a15ec6b6
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-02-10 15:21:31 -0800

    add a functional forEach loop that has an if else statement, which will convert the inputted string into numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,99 @@
+// Business logic:
+
+
+function convert(variables) {
+
+  const newArray = [];
+
+  for (let i = 0; i <= variables; i++) {
+    debugger;
+    if ([i].includes("I")) {
+      newArray.push(1);
+    }
+    else if ([i].includes("V")) {
+      newArray.push(5);
+    }
+    else {
+      newArray.push(0);
+    }
+  }
+
+  return newArray;
+
+};
+
+/*function addThem(secondArray) {
+  for (let i = 0; i <= secondArray; i++) {
+    sum += [i];
+  }
+  return sum;
+};*/
+
+// UI logic:
 $(document).ready(function(){
-  $("#formOne").submit(function(event){
+  $("#formOne").submit(function(event) {
     event.preventDefault();
-    const nameInput = $("input#name").val();
-    const emailInput = $("input#email").val();
-    const bandNameInput = $("input#bandName").val();
-    const budgetInput = $("input#budget").val();
-    const datesInput = $("input#dates").val();
-    const lookingFor = $("input:radio[name=lookingFor]:checked").val();
+  
+    const input = $("input#sentence").val();
     
-    $("#submitButton").hide();
-    $("#thankyou").show();
+    const array = input.toUpperCase().split("");
+    
+    console.log(array);
+
+    let newArray = [];
+    
+    array.forEach(function(number) {
+      debugger;
+      if (number === "I") {
+        newArray.push(1);
+      }
+      else if (number === "V") {
+          newArray.push(5);
+      }
+      else {
+            newArray.push(0);
+     }
+     return newArray;
+    });
+
+    const numberConvert = convert(array);
+
+    console.log(newArray);
   });
-});
+});    
+
+/* function convert(variables) {
+
+      let newArray = [];
+
+      for (let i = 0; i <= variables; i++) {
+        debugger;
+        if ([i] === "I")) {
+          newArray.push(1);
+        }
+        else if ([i] === "V")) {
+          newArray.push(5);
+        }
+        else {
+          newArray.push(0);
+        }
+      }
+
+      return newArray;
+
+    };
+
+   
+
+    console.log(numberConvert);
+
+    //const secondArray = convert(array);
+
+    //console.log(secondArray);
+
+    //const result = addThem(secondArray);
+
+    //$("#reveal").show(text(numberConvert));
+
+  });
+});*/
